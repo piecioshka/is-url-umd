379f49ead254a4fb306c288ab56903bf2a0d8ea8
Piotr Kowalski <piecioshka@gmail.com>
2016-05-05 10:31:28 +0200

    Support dev & prod versions

--- dist/is-url-umd.js
+++ dist/is-url-umd.js
@@ -2,7 +2,7 @@
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
-		define([], factory);
+		define("isURL", [], factory);
 	else if(typeof exports === 'object')
 		exports["isURL"] = factory();
 	else
@@ -11,41 +11,41 @@
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -54,6 +54,8 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	'use strict';
+	
 	// Simple CommonJS export, but Webpack configuration can exports it to 3 formats:
 	//  - CommonJS
 	//  - AMD
@@ -69,22 +71,22 @@ return /******/ (function(modules) { // webpackBootstrap
 	/**
 	 * Expose `isUrl`.
 	 */
-
+	
 	module.exports = isUrl;
-
+	
 	/**
 	 * Matcher.
 	 */
-
+	
 	var matcher = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/;
-
+	
 	/**
 	 * Loosely validate a URL `string`.
 	 *
 	 * @param {String} string
 	 * @return {Boolean}
 	 */
-
+	
 	function isUrl(string){
 	  return matcher.test(string);
 	}
@@ -94,3 +96,4 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ ])
 });
 ;
+//# sourceMappingURL=is-url-umd.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 'use strict';
 
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './index.js',
+    entry: {
+        'is-url-umd': './index.js',
+        'is-url-umd.min': './index.js'
+    },
+
+    devtool: 'source-map',
 
     output: {
         library: 'isURL',
         libraryTarget: 'umd',
-
-        filename: 'is-url.js',
+        umdNamedDefine: true,
+        filename: '[name].js',
         path: 'dist/'
-    }
+    },
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true
+        })
+    ]
 };
